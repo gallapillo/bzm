70640513f97a1a10221e921cebc732c287a0040e
gallapillo <golokmi@gmail.com>
2024-04-28 19:17:50 +0700

    Added a Product entity

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from shop import models
 # Register your models here.
+
+admin.site.register(models.Product)
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+# TODO: Добавить категории + добавить валюту + еще что-нибудь
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название товара")
+    price = models.FloatField(verbose_name="Цена")
+    description = models.CharField(max_length=200, verbose_name="Описание товара")
+    discount = models.IntegerField(default=0, verbose_name="Скидка")
+    in_stock = models.BooleanField(default=True, verbose_name="В наличии")
+    draft = models.BooleanField(default=False, verbose_name="Черновик")
 
